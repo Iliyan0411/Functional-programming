5747a854e70e2a2b80513ac667ddb3952efa8fdd
Iliyan0411 <ilian_dd00@abv.bg>
2020-11-22 13:58:23 +0200

    Added new solved problems

--- FirstTest_prepare/first-test-prepare.rkt
+++ FirstTest_prepare/first-test-prepare.rkt
@@ -10,3 +10,44 @@
   (cond [(> a b) 0]
         [(to-low? a) (+ a (sum-numbers (+ a 1) b))]
         [else (sum-numbers (+ a 1) b)]))
+
+
+;Problem 4
+(define (repeater str)
+  (λ (count glue)
+    (define (helper n result)
+      (if (= n 0)
+          result
+          (helper (- n 1) (string-append result str glue))))
+    (helper count "")))
+
+
+;Problem 5
+(define (kratna-sum? n d)
+    (define (helper sum n)
+      (cond [(and (= n 0) (= (remainder sum d) 0)) #t]
+            [(> n 0) (helper (+ sum (remainder n 10)) (quotient n 10))]
+            [else #f]))
+    (helper 0 n))
+
+(define (sum-digits a b k)
+  (define (helper sum a)
+    (cond [(> a b) sum]
+          [(kratna-sum? a k) (helper (+ sum a) (+ a 1))]
+          [else (helper sum (+ a 1))]))
+  (helper 0 a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
