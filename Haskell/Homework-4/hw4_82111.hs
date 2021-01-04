52a96e63269391328a059ab74ab3766c73feab1d
Iliyan0411 <ilian_dd00@abv.bg>
2021-01-04 15:27:46 +0200

    Problem_1 - solved

--- Haskell/Homework-4/hw4_82111.hs
+++ Haskell/Homework-4/hw4_82111.hs
@@ -16,14 +16,14 @@ type Person = (Int, String, String)
 ----- a)
 getAverageBalance :: ([Account],[Person]) -> (Person -> Bool) -> Double
 getAverageBalance ([], []) _ = 0
-getAverageBalance ([], [_]) _ = error "Invalid arguments"
-getAverageBalance ([_], []) _ = error "Invalid arguments"
+getAverageBalance ([], [_]) _ = 0
+getAverageBalance ([_], []) _ = 0
 getAverageBalance dataBase pred = helper dataBase 0 0
     where
         -- helper-функция реализираща getAverageBalance итеративно
-        helper :: ([Account],[Person]) -> Double -> Double ->Double
         helper dataBase result div
-            | null (snd dataBase) = result / div
+            | null (snd dataBase) && result == 0 = 0
+            | null (snd dataBase) && result > 0 = result / div
             | pred currPerson = helper (fst dataBase, tail (snd dataBase)) 
                                         (result + fst (balance (fst3 currPerson) (fst dataBase)))
                                         (div + snd (balance (fst3 currPerson) (fst dataBase)))
@@ -33,19 +33,48 @@ getAverageBalance dataBase pred = helper dataBase 0 0
 
                     -- връща двойка от сумата на парите във всички сметки на човека в дадената банка 
                     -- и съответно броят на сметките, които той притежава
-                    balance :: Int -> [Account] -> (Double, Double)
                     balance _ [] = (0, 0)
                     balance personID accounts = helper accounts 0 0
                         where
                             -- helper-функция реализираща balance итеративно
-                            helper :: [Account] -> Double -> Double -> (Double, Double)
                             helper accounts result counter
                                 | null accounts = (result, counter)
                                 | personID == snd3 (head accounts) = helper (tail accounts) 
                                                                             (result + thd3 (head accounts))
                                                                             (counter + 1)
                                 | otherwise = helper (tail accounts) result counter
----------------------------------------------------------------------------------------------------------------------------------------
+
+
+----- b)
+type Balance = Double
+
+averageBalanceOfCities :: ([Account],[Person]) -> [String] -> Balance
+averageBalanceOfCities ([], []) [_] = 0
+averageBalanceOfCities ([_], []) [_] = 0
+averageBalanceOfCities ([], [_]) [_] = 0
+averageBalanceOfCities ([_], [_]) [] = 0
+averageBalanceOfCities dataBase cities = helper cities 0 0
+    where
+        -- helper-функция реализираща averageBalanceOfCities итеративно
+        helper cities result div
+            | null cities && result == 0 = 0
+            | null cities && result > 0 = result / div
+            | otherwise = helper (tail cities) 
+                                 (result + getAverageBalance dataBase (\ (_,_,city) -> city == head cities) * k)
+                                 (div + k)
+                where   
+                    -- k е съкратено наименования на функцията count, дефинирана по-долу
+                    k = fromIntegral (count (snd dataBase) (head cities))
+
+                    -- count представлява броят на сметките, които имат хората от текущия града
+                    count [] _ = 0
+                    count persons city = if thd3 (head persons) /= city
+                                         then count (tail persons) city
+                                         else length [acc | acc <- fst dataBase, fst3 (head persons) == snd3 acc] 
+                                              + count (tail persons) city
+
+        
+------------------------------------------ ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 -- inputs
@@ -55,10 +84,22 @@ people1 = [(1,"Ivan","Sofia"),(2,"Georgi","Burgas"), (3,"Petar","Plovdiv"),(4,"P
 accounts1 :: [Account]
 accounts1 = [(1,1,12.5),(2,1,123.2),(3,2,13.0),(4,2,50.2),(5,2,17.2), (6,3,18.3),(7,4,19.4)]
 
-
 ---------------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------
 main :: IO()
 main = do
+    print "----- Problem_1 -----"
+    print "----- a)"
     print (getAverageBalance (accounts1,people1) (\ (_,_,city) -> city == "Burgas")) -- --> 24.950000000000003
-    print (getAverageBalance (accounts1,people1) (\ (_,(n:_),_) -> n == 'P')) -- --> 18.85
+    print (getAverageBalance (accounts1,people1) (\ (_,n:_,_) -> n == 'P')) -- --> 18.85
+
+    print "------------------------------------------------------------------------------"
+
+    print "----- b)"
+    print (averageBalanceOfCities (accounts1,people1) ["Sofia","Gabrovo","Stara Zagora"])-- --> 67.85
+    print (averageBalanceOfCities (accounts1,people1) ["Burgas","Gabrovo","Plovdiv"])-- --> 23.62 = (13 + 50.2 + 17.2 + 19.4 + 18.3) / 5
+    
+    print "------------------------------------------------------------------------------"
+    print "------------------------------------------------------------------------------"
+
+    print "----- Problem_2 -----"
