70e9b41400770d439b3d60151b83fe46c41e73f7
Iliyan0411 <ilian_dd00@abv.bg>
2021-01-05 15:01:28 +0200

    ex1 & ex2 ready

--- Haskell/Homework-4/hw4_82111.hs
+++ Haskell/Homework-4/hw4_82111.hs
@@ -6,9 +6,8 @@ snd3 (_,b,_) = b
 
 thd3 :: (a,b,c) -> c -- връща третия елемент на вектор с размер 3
 thd3 (_,_,c) = c
----------------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------------
-
+-------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------
 ----- Problem_1 -----
 type Account = (Int, Int, Double)
 type Person = (Int, String, String)
@@ -18,32 +17,19 @@ getAverageBalance :: ([Account],[Person]) -> (Person -> Bool) -> Double
 getAverageBalance ([], []) _ = 0
 getAverageBalance ([], [_]) _ = 0
 getAverageBalance ([_], []) _ = 0
-getAverageBalance dataBase pred = helper dataBase 0 0
+getAverageBalance (accounts, persons) pred = helper persons 0 0
     where
-        -- helper-функция реализираща getAverageBalance итеративно
-        helper dataBase result div
-            | null (snd dataBase) && result == 0 = 0
-            | null (snd dataBase) && result > 0 = result / div
-            | pred currPerson = helper (fst dataBase, tail (snd dataBase)) 
-                                        (result + fst (balance (fst3 currPerson) (fst dataBase)))
-                                        (div + snd (balance (fst3 currPerson) (fst dataBase)))
-            | otherwise = helper (fst dataBase, tail (snd dataBase)) result div
+        helper [] 0 _ = 0
+        helper persons result div
+            | null persons = result / div
+            | pred (head persons) = helper (tail persons) 
+                                           (result + fst(wantedAcc (head persons))) 
+                                           (div + snd(wantedAcc (head persons)))
+            | otherwise = helper (tail persons) result div
                 where
-                    currPerson = head (snd dataBase) -- текущият човек, който разглеждаме от базата данни
-
-                    -- връща двойка от сумата на парите във всички сметки на човека в дадената банка 
-                    -- и съответно броят на сметките, които той притежава
-                    balance _ [] = (0, 0)
-                    balance personID accounts = helper accounts 0 0
+                    wantedAcc person = (sum pAcc, fromIntegral (length pAcc))
                         where
-                            -- helper-функция реализираща balance итеративно
-                            helper accounts result counter
-                                | null accounts = (result, counter)
-                                | personID == snd3 (head accounts) = helper (tail accounts) 
-                                                                            (result + thd3 (head accounts))
-                                                                            (counter + 1)
-                                | otherwise = helper (tail accounts) result counter
-
+                            pAcc = [thd3 acc | acc <- accounts, snd3 acc == fst3 person]
 
 ----- b)
 type Balance = Double
@@ -53,30 +39,13 @@ averageBalanceOfCities ([], []) [_] = 0
 averageBalanceOfCities ([_], []) [_] = 0
 averageBalanceOfCities ([], [_]) [_] = 0
 averageBalanceOfCities ([_], [_]) [] = 0
-averageBalanceOfCities dataBase cities = helper cities 0 0
+averageBalanceOfCities (accounts, persons) cities = average 
     where
-        -- helper-функция реализираща averageBalanceOfCities итеративно
-        helper cities result div
-            | null cities && result == 0 = 0
-            | null cities && result > 0 = result / div
-            | otherwise = helper (tail cities) 
-                                 (result + getAverageBalance dataBase (\ (_,_,city) -> city == head cities) * k)
-                                 (div + k)
-                where   
-                    -- k е съкратено наименования на функцията count, дефинирана по-долу
-                    k = fromIntegral (count (snd dataBase) (head cities))
+        wantedPersons = [prs | prs <- persons, length (filter (== thd3 prs) cities) > 0]
+        allAcc = [thd3 acc | acc <- accounts, length (filter (\x -> fst3 x==snd3 acc) wantedPersons)>0]
+        average = sum allAcc / fromIntegral (length allAcc)
 
-                    -- count представлява броят на сметките, които имат хората от текущия града
-                    count [] _ = 0
-                    count persons city = if thd3 (head persons) /= city
-                                         then count (tail persons) city
-                                         else length [acc | acc <- fst dataBase, fst3 (head persons) == snd3 acc] 
-                                              + count (tail persons) city
-
-        
----------------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------------
-
+----------------------------------------------------------------------------------------------------
 -- inputs
 people1 :: [Person]
 people1 = [(1,"Ivan","Sofia"),(2,"Georgi","Burgas"), (3,"Petar","Plovdiv"),(4,"Petya","Burgas")]
@@ -84,8 +53,34 @@ people1 = [(1,"Ivan","Sofia"),(2,"Georgi","Burgas"), (3,"Petar","Plovdiv"),(4,"P
 accounts1 :: [Account]
 accounts1 = [(1,1,12.5),(2,1,123.2),(3,2,13.0),(4,2,50.2),(5,2,17.2), (6,3,18.3),(7,4,19.4)]
 
+
+t1 :: BTree
+t1 = Node 16 (Node 0 Empty Empty)
+    (Node 4 (Node 1 Empty Empty)
+    (Node 0 Empty Empty))
+
+t2 :: BTree
+t2 = Node 4 (Node 0 Empty Empty)
+    (Node 2 (Node 1 Empty Empty) 
+    Empty) 
+----------------------------------------------------------------------------------------------------
+
+----- Problem_2 -----
+data BTree = Empty | Node Int BTree BTree
+
+countInteresting :: BTree -> Int
+countInteresting Empty = 0
+countInteresting (Node 1 Empty Empty) = 1
+countInteresting (Node 2 Empty t2) = 1 + countInteresting t2
+countInteresting (Node 2 t1 Empty) = 1 + countInteresting t1
+countInteresting (Node 4 (Node m t11 t12) (Node n t21 t22)) = 1 +
+                                                              countInteresting (Node m t11 t12) + 
+                                                              countInteresting (Node n t21 t22)
+countInteresting (Node _ t1 t2) = countInteresting t1 + countInteresting t2
+
 ---------------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------
+
 main :: IO()
 main = do
     ----- Problem_1 -----
@@ -93,11 +88,14 @@ main = do
     print (getAverageBalance (accounts1,people1) (\ (_,_,city) -> city == "Burgas")) -- --> 24.950000000000003
     print (getAverageBalance (accounts1,people1) (\ (_,n:_,_) -> n == 'P')) -- --> 18.85
 
+    print ("------------")
+
     ----- b)
     print (averageBalanceOfCities (accounts1,people1) ["Sofia","Gabrovo","Stara Zagora"])-- --> 67.85
-    print (averageBalanceOfCities (accounts1,people1) ["Burgas","Gabrovo","Plovdiv"])-- --> 23.62 = (13 + 50.2 + 17.2 + 19.4 + 18.3) / 5
-    
-    print "------------------------------------------------------------------------------"
-    print "------------------------------------------------------------------------------"
+    print (averageBalanceOfCities (accounts1,people1) ["Burgas","Gabrovo","Plovdiv"])-- --> 23.62
+
+    print ("------------")
 
     ----- Problem_2 -----
+    print (countInteresting t1) -- -> 2 (4=2^2, 1=2^0)
+    print (countInteresting t2) -- -> 3 (4=2^2, 2=2^1, 1=2^0)
